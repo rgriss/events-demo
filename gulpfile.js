3204b40b1ab4ab0eca9e36cc7505b36ef15d6b07
Ryan Grissinger <ryan.grissinger@gmail.com>
2016-04-07 19:48:06 -0400

    bower & composer changes

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,14 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss');
+
+ mix.scripts([
+  '../lib/jquery/dist/jquery.min.js',
+  '../lib/bootstrap/dist/js/bootstrap.js',
+  'app.js'
+ ], 'public/js/app.js');
+
+
 });
+
+
